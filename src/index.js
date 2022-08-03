63b78b80fc995089f152498050b052dc31d00a01
VKlapan <vladimir.klapan@gmail.com>
2022-08-03 20:37:11 +0300

    add test functions to test last page feature

--- src/index.js
+++ src/index.js
@@ -17,13 +17,14 @@ const offReadMoreBtn = () => refs.btnMoreEl.classList.add('invisible');
 const onReadMoreBtn = () => refs.btnMoreEl.classList.remove('invisible');
 
 let query = '';
-let currentPage = 1;
+const per_page = 40;
+let currentPage = 12;
 
 offReadMoreBtn();
 
 const fetchPics = query => {
   return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&per_page=40`
+    `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&${per_page}`
   )
     .then(r => r.json())
     .then(r => r.hits)
@@ -88,6 +89,7 @@ const startSearch = event => {
 };
 
 const loadMore = () => {
+  offReadMoreBtn();
   fetchPics(query)
     .then(renderMarkupGallery)
     .then(addGallery)
@@ -96,3 +98,68 @@ const loadMore = () => {
 
 refs.btnSearchEl.addEventListener('click', startSearch);
 refs.btnMoreEl.addEventListener('click', loadMore);
+
+// -------------------------------------------------------------------
+
+const fetchPicsTest = query => {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&per_page=${per_page}`
+  )
+    .then(r => r.json())
+    .then(r => {
+      const hitsArr = r.hits;
+      if (hitsArr.length === 0) {
+        return Promise.reject('not found');
+      } else {
+        return {
+          galleryArr: hitsArr,
+          totalHits: r.totalHits,
+        };
+      }
+    });
+};
+
+const renderMarkupGalleryTest = ({ galleryArr, totalHits }) => {
+  const markupGallery = galleryArr
+    .map(img => {
+      return `
+  <div class="photo-card">
+       <a href="${img.largeImageURL}">
+        <img class="gallery__image" src="${img.webformatURL}" alt="" loading="lazy" />
+        </a>
+      <div class="info">
+            <p class="info-item">
+                <b>Likes<br>${img.likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views<br>${img.views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments<br>${img.comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads<br>${img.downloads}</b>
+            </p>
+      </div>
+  </div>
+      `;
+    })
+    .join('');
+  currentPage += 1;
+  const isLastRender = currentPage > Math.floor(totalHits / per_page);
+  return { isLastRender, markupGallery };
+};
+
+const addGalleryTest = ({ isLastRender, markupGallery }) => {
+  refs.galleryEl.insertAdjacentHTML('beforeend', markupGallery);
+  if (!isLastRender) {
+    onReadMoreBtn();
+  }
+};
+
+fetchPicsTest('trasdrtey')
+  .then(renderMarkupGalleryTest)
+  .then(addGalleryTest)
+  .then(createGallery)
+  .catch(console.log);
+//(currentPage <= Math.floor(r.totalHits / per_page)
