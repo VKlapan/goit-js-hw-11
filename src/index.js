b21d3d8b971ca9a7bfd48d20fc8d493e919a397b
VKlapan <vladimir.klapan@gmail.com>
2022-08-04 16:11:32 +0300

    add no-more-pages feature

--- src/index.js
+++ src/index.js
@@ -11,97 +11,25 @@ const refs = {
   btnSearchEl: formEl.querySelector('button'),
   galleryEl: document.querySelector('.gallery'),
   btnMoreEl: document.querySelector('.load-more'),
+  noMoreMessageEl: document.querySelector('.no-more-pages'),
 };
 
 const offReadMoreBtn = () => refs.btnMoreEl.classList.add('invisible');
 const onReadMoreBtn = () => refs.btnMoreEl.classList.remove('invisible');
 
+const offNoMorePagesMessage = () =>
+  refs.noMoreMessageEl.classList.add('invisible');
+const onNoMorePagesMessage = () =>
+  refs.noMoreMessageEl.classList.remove('invisible');
+
 let query = '';
 const per_page = 40;
-let currentPage = 12;
+let currentPage = null;
 
 offReadMoreBtn();
+offNoMorePagesMessage();
 
 const fetchPics = query => {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&${per_page}`
-  )
-    .then(r => r.json())
-    .then(r => r.hits)
-    .catch(console.log);
-};
-
-const renderMarkupGallery = galleryArr => {
-  const markupGallery = galleryArr
-    .map(img => {
-      return `
-  <div class="photo-card">
-       <a href="${img.largeImageURL}">
-        <img class="gallery__image" src="${img.webformatURL}" alt="" loading="lazy" />
-        </a>
-      <div class="info">
-            <p class="info-item">
-                <b>Likes<br>${img.likes}</b>
-            </p>
-            <p class="info-item">
-            <b>Views<br>${img.views}</b>
-            </p>
-            <p class="info-item">
-            <b>Comments<br>${img.comments}</b>
-            </p>
-            <p class="info-item">
-            <b>Downloads<br>${img.downloads}</b>
-            </p>
-      </div>
-  </div>
-      `;
-    })
-    .join('');
-  currentPage += 1;
-  return markupGallery;
-};
-
-const addGallery = markupGallery => {
-  refs.galleryEl.insertAdjacentHTML('beforeend', markupGallery);
-  onReadMoreBtn();
-};
-
-const createGallery = () => {
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-  });
-
-  lightbox.on('show.simplelightbox');
-};
-
-const startSearch = event => {
-  event.preventDefault();
-  refs.galleryEl.innerHTML = '';
-  query = refs.searchInputEl.value;
-  console.log(refs.searchInputEl.value);
-
-  fetchPics(query)
-    .then(renderMarkupGallery)
-    .then(addGallery)
-    .then(createGallery);
-};
-
-const loadMore = () => {
-  offReadMoreBtn();
-  fetchPics(query)
-    .then(renderMarkupGallery)
-    .then(addGallery)
-    .then(createGallery);
-};
-
-refs.btnSearchEl.addEventListener('click', startSearch);
-refs.btnMoreEl.addEventListener('click', loadMore);
-
-// -------------------------------------------------------------------
-
-const fetchPicsTest = query => {
   return fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&per_page=${per_page}`
   )
@@ -118,8 +46,7 @@ const fetchPicsTest = query => {
       }
     });
 };
-
-const renderMarkupGalleryTest = ({ galleryArr, totalHits }) => {
+const renderMarkupGallery = ({ galleryArr, totalHits }) => {
   const markupGallery = galleryArr
     .map(img => {
       return `
@@ -150,16 +77,94 @@ const renderMarkupGalleryTest = ({ galleryArr, totalHits }) => {
   return { isLastRender, markupGallery };
 };
 
-const addGalleryTest = ({ isLastRender, markupGallery }) => {
+const addGallery = ({ isLastRender, markupGallery }) => {
   refs.galleryEl.insertAdjacentHTML('beforeend', markupGallery);
   if (!isLastRender) {
     onReadMoreBtn();
+  } else {
+    onNoMorePagesMessage();
   }
 };
 
-fetchPicsTest('trasdrtey')
-  .then(renderMarkupGalleryTest)
-  .then(addGalleryTest)
-  .then(createGallery)
-  .catch(console.log);
-//(currentPage <= Math.floor(r.totalHits / per_page)
+const createGallery = () => {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+  lightbox.on('show.simplelightbox');
+};
+
+const startSearch = event => {
+  event.preventDefault();
+  currentPage = 1;
+  offReadMoreBtn();
+  offNoMorePagesMessage();
+
+  refs.galleryEl.innerHTML = '';
+  query = refs.searchInputEl.value;
+  console.log(refs.searchInputEl.value);
+
+  fetchPics(query)
+    .then(renderMarkupGallery)
+    .then(addGallery)
+    .then(createGallery);
+};
+
+const loadMore = () => {
+  offReadMoreBtn();
+  fetchPics(query)
+    .then(renderMarkupGallery)
+    .then(addGallery)
+    .then(createGallery);
+};
+
+refs.btnSearchEl.addEventListener('click', startSearch);
+refs.btnMoreEl.addEventListener('click', loadMore);
+
+// -------------------------------------------------------------------
+
+// const renderMarkupGalleryOld = galleryArr => {
+//   const markupGallery = galleryArr
+//     .map(img => {
+//       return `
+//   <div class="photo-card">
+//        <a href="${img.largeImageURL}">
+//         <img class="gallery__image" src="${img.webformatURL}" alt="" loading="lazy" />
+//         </a>
+//       <div class="info">
+//             <p class="info-item">
+//                 <b>Likes<br>${img.likes}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Views<br>${img.views}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Comments<br>${img.comments}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Downloads<br>${img.downloads}</b>
+//             </p>
+//       </div>
+//   </div>
+//       `;
+//     })
+//     .join('');
+//   currentPage += 1;
+//   return markupGallery;
+// };
+
+// const addGalleryOld = markupGallery => {
+//   refs.galleryEl.insertAdjacentHTML('beforeend', markupGallery);
+//   onReadMoreBtn();
+// };
+
+//   const fetchPicsOld = query => {
+//     return fetch(
+//       `${BASE_URL}?key=${API_KEY}&q=${query}&${PARAMS}&page=${currentPage}&${per_page}`
+//     )
+//       .then(r => r.json())
+//       .then(r => r.hits)
+//       .catch(console.log);
+//   };
